1bcc42283a0871400b1aaa0bf1508361801732f2
henna-h <henna.honkanen1@gmail.com>
2021-09-26 20:43:38 +0300

    my_react_app: filterFunction

--- web/modules/custom/my_react_app/js/src/index.js
+++ web/modules/custom/my_react_app/js/src/index.js
@@ -16,12 +16,7 @@ const Root = () => {
   const [filter, setFilter] = useState({countries: countries, availableOnly: availabilityChecked})
   console.log(filter)
   console.log("countries: " + countries)
-
-
-  useEffect(() => {
-
-  })
-
+  console.log("availabilityChecked: " + availabilityChecked)
 
   const handleCountryChange = e => {
     
@@ -35,6 +30,17 @@ const Root = () => {
     setAvailabilityChecked(!availabilityChecked)
     setFilter({ ...filter, availableOnly: !availabilityChecked })
     //console.log(filter)
+  }
+
+  const filterFunction = (hotel) => {
+    if(!filter.countries.includes(hotel.country)){
+      return false
+
+    } else if(availabilityChecked == true && hotel.isAvailable == false){
+      return false
+    }
+
+    return true
   }
 
 
@@ -57,7 +63,7 @@ const Root = () => {
       </input>
       <label for="available">Is available</label>
 
-      {hotels.filter(hotel => filter.countries.includes(hotel.country)).map(hotel => (
+      {hotels.filter(hotel => filterFunction(hotel)).map(hotel => (
         <div key={hotel.id}>
           <p>{hotel.name}</p>
         </div>
