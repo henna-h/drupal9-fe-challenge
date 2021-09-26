c0245ab210ed25213b08a337f984e3e493f773b1
henna-h <henna.honkanen1@gmail.com>
2021-09-26 23:10:13 +0300

    my_react_app: styled components for hotel component

--- web/modules/custom/my_react_app/js/src/components/Hotel.js
+++ web/modules/custom/my_react_app/js/src/components/Hotel.js
@@ -1,20 +1,63 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const HotelBox = styled.div`
+    background: Gainsboro;
+    height: 17em;
+    margin-bottom: 2em;
+    padding: 1em;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between
+`
+
+const HotelImage = styled.img`
+    height: 15em;
+    width: 15em;
+    display: inline-block;
+    float: left;
+`
+
+const HotelMiddleColumn = styled.div`
+    float: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-center;
+`
+
+const HotelLeftColumn = styled.div`
+    float: right;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+`
+
+const Span = styled.div`
+    display: flex;
+    flex-direction: row;
+`
 
 const Hotel = ({ hotel }) => {
 
     return (
-        <div key={hotel.id}>
-          <img src={hotel.imageUrl} alt={hotel.name} height="200" width="200" />
-          <h3>{hotel.name}</h3>
-          <p>{hotel.city}, {hotel.country}</p>
+        <HotelBox key={hotel.id}>
+          <HotelImage src={hotel.imageUrl} alt={hotel.name} />
 
-          <p>Available: {hotel.isAvailable == true ? <p>Yes</p> : <p>No</p>}</p>
+          <HotelMiddleColumn>
+            <h3>{hotel.name}</h3>
+            <p>{hotel.city}, {hotel.country}</p>
 
-          <p>Swimming pool: {hotel.hasSwimmingPool == true ? <p>Yes</p> : <p>No</p>}</p>
+            <Span><p>Available: </p> {hotel.isAvailable == true ? <p> Yes</p> : <p> No</p>}</Span>
 
-          <h2>{hotel.price}</h2>
+            <Span><p>Swimming pool: </p> {hotel.hasSwimmingPool == true ? <p> Yes</p> : <p> No</p>}</Span>
 
-        </div>
+          </HotelMiddleColumn>
+        
+          <HotelLeftColumn>
+            <h2>{hotel.price}</h2>
+          </HotelLeftColumn>
+
+        </HotelBox>
     )
 }
 
