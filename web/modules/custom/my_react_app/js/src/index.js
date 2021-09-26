bacaf23118f5cea5977576b16da3b818dbdf8edf
henna-h <henna.honkanen1@gmail.com>
2021-09-26 20:26:11 +0300

    react app: country filter

--- web/modules/custom/my_react_app/js/src/index.js
+++ web/modules/custom/my_react_app/js/src/index.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
+import Select from 'react-select'
 
 const Root = () => {
+  console.log(drupalSettings.hotels)
+  const allHotels = drupalSettings.hotels
+
+  const hotelCountries = Array.from(new Set(allHotels.map(hotel => hotel.country)))
+  console.log(hotelCountries)
+
+  const [hotels, setHotels] = useState(allHotels)
+  const [countries, setCountries] = useState(hotelCountries)
+  const [availabilityChecked, setAvailabilityChecked] = useState(false)
+
+  const [filter, setFilter] = useState({countries: countries, availableOnly: availabilityChecked})
+  console.log(filter)
+  console.log("countries: " + countries)
+
+
+  useEffect(() => {
+
+  })
+
+
+  const handleCountryChange = e => {
+    
+    console.log(e)
+    setCountries([e.target.value])
+    setFilter({ ...filter, countries: e.target.value })
+    //console.log(hotels)
+  }
+
+  const handleAvailabilityChange = () => {
+    setAvailabilityChecked(!availabilityChecked)
+    setFilter({ ...filter, availableOnly: !availabilityChecked })
+    //console.log(filter)
+  }
+
+
   return (
-    <>Hello world.</>
+    <div>
+      <select onChange={handleCountryChange}>
+        <option value={hotelCountries} selected="selected"></option>
+        {hotelCountries.map(hotelCountry=>(
+          <option value={[hotelCountry]}>{hotelCountry}</option>
+        ))}
+      </select>
+
+      <input 
+        type="checkbox"
+        id="available"
+        name="available"
+        checked={availabilityChecked}
+        onChange={handleAvailabilityChange}
+      >
+      </input>
+      <label for="available">Is available</label>
+
+      {hotels.filter(hotel => filter.countries.includes(hotel.country)).map(hotel => (
+        <div key={hotel.id}>
+          <p>{hotel.name}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
